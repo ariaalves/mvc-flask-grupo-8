da6f0c5235e7e9bcc953fc4076397cfd2e5e5491
Ariany Alves <ariariany093@gmail.com>
2025-09-08 00:27:22 -0300

    atualizando arquivos na view

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -2,7 +2,6 @@ from flask import render_template, request, redirect, url_for
 from models.user import User, db
 
 class UserController:
-    # A chamada para esse método seria feita diretamente pela classe, sem a necessidade de criar um objeto (uma instância):
     @staticmethod
     def list_users():
         users = User.query.all()
@@ -13,8 +12,6 @@ class UserController:
         if request.method == 'POST':
             name = request.form['name']
             email = request.form['email']
-
-            # validações simples: se usuário já existe no db, etc
 
             new_user = User(name=name, email=email)
             db.session.add(new_user)
--- models/user.py
+++ models/user.py
@@ -1,11 +1,9 @@
 from . import db
 
-# Cada classe que herda de db.Model se torna uma tabela no banco de dados.
-class User(db.Model): # Declaração de um novo modelo/tabela: User
-    # define o nome da tabela no banco de dados para a classe User como "users", como boas práticas
+class User(db.Model): 
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True) # Define uma coluna ... 
+    id = db.Column(db.Integer, primary_key=True) 
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
